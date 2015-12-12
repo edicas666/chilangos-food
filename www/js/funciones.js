acc16dd0df3c8cbea6d689f1725bc08665932e80
edicas666 <edicas666@gmail.com>
2015-12-12 01:56:09 -0600

    incremento de iconos
    se incrementaron los tamaos de iconos de menu principal

--- www/js/funciones.js
+++ www/js/funciones.js
@@ -6,10 +6,10 @@ function clicklogo() {
     var visita = document.getElementById("imgv");
     
   
-    menu.className = "col-xs-4 animated bounceIn retraso-2";
-    pedidos.className = "col-xs-4 col-xs-offset-4 animated bounceIn retraso-2";
-    promo.className = "col-xs-4 animated bounceIn retraso-2";
-    visita.className = "col-xs-4 col-xs-offset-4 animated bounceIn retraso-2";
+    menu.className = "col-xs-5 animated bounceIn retraso-2";
+    pedidos.className = "col-xs-5 col-xs-offset-2 animated bounceIn retraso-2";
+    promo.className = "col-xs-6 animated bounceIn retraso-2";
+    visita.className = "col-xs-5 col-xs-offset-1 animated bounceIn retraso-2";
     
     menu.style.display = "block";
     pedidos.style.display = "block";
