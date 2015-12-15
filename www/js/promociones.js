b4a2afe992b51c1e90165ed61a28acb3badb6cf4
Luis Marti Godinez Sánchez <luismarti07@gmail.com>
2015-12-15 02:40:55 -0600

    Promociones listas

---
+++ www/js/promociones.js
@@ -0,0 +1,8 @@
+function mostrar(){
+var n = document.getElementsByClassName("otras");
+    n[0].style.display = "block";
+    n[1].style.display = "block";
+    n[2].style.display = "block";
+    
+}
+  
